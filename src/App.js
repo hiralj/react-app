efc1f12e7d25b6db78f86ff3cfb6b69dbc4796cd
Hiral Jhaveri <hiraljhaveri7@gmail.com>
2024-05-21 17:43:26 +0530

    cosmetic change

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-	        Edit <code>src/App.js</code> and publish to GitHub
+	        Edited <code>src/App.js</code> and publish to GitHub
         </p>
         <a
           className="App-link"
