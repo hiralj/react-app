848997d5a874b00dfa4465e4600a58658de7d1e7
Niyati Jhaveri <niyati@Niyatis-MacBook-Air.local>
2024-05-21 16:35:08 +0530

    My initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://sta7bbda60.execute-api.us-east-1.amazonaws.com/dev/person');
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(data);
+  console.log(typeof data);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+	        Edited <code>src/App.js</code> in real time again
         </p>
         <a
           className="App-link"
@@ -17,6 +36,12 @@ function App() {
         >
           Learn React
         </a>
+      <div>
+        <h3>Items</h3>
+        <ul>
+          <li> {data && data.Username && data.Username.S} </li>
+        </ul> 
+      </div>
       </header>
     </div>
   );
